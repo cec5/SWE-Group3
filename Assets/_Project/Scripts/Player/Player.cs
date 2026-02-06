81cb03dcb58afb692545505a4279d24977152a85
Christopher Cortez <chris06.cortez@gmail.com>
2026-02-06 11:32:36 +0000

    Sprinting implemented, crouching is in the script but needs refinement

--- Assets/_Project/Scripts/Player/Player.cs
+++ Assets/_Project/Scripts/Player/Player.cs
@@ -1,26 +1,37 @@
-using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+    private BoxCollider2D cd;
 
     [Header("Movement Settings")]
     private float xInput;
-    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float runSpeed = 6f;
+    [SerializeField] private float crouchSpeed = 2f;
+    private float currentSpeed;
+
     [SerializeField] private float jumpForce = 12f;
     private bool facingRight = true;
 
+    [Header("States")]
+    private bool isRunning;
+    private bool isCrouching;
+
     [Header("Collision Details")]
     [SerializeField] private float groundCheckDistance = 1.4f;
+    [SerializeField] private float ceilingCheckDistance = 1.0f;
     [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded;
+    private bool headBlocked;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
+        cd = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -32,17 +43,24 @@ public class Player : MonoBehaviour
         HandleFlip();
     }
 
-    private void HandleAnimations()
-    {
-        anim.SetFloat("xVelocity", rb.linearVelocity.x);
-        anim.SetFloat("yVelocity", rb.linearVelocity.y);
-        anim.SetBool("isGrounded", isGrounded);
-    }
-
     private void HandleInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        bool crouchInput = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+
+        if (crouchInput && isGrounded)
+        {
+            isCrouching = true;
+        }
+        else if (!headBlocked) // Only stop crouching if there's space above
+        {
+            isCrouching = false;
+        }
+
+        if (!isCrouching && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
         {
             Jump();
         }
@@ -50,7 +68,22 @@ public class Player : MonoBehaviour
 
     private void HandleMovement()
     {
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        if (isCrouching)
+            currentSpeed = crouchSpeed;
+        else if (isRunning)
+            currentSpeed = runSpeed;
+        else
+            currentSpeed = walkSpeed;
+
+        rb.linearVelocity = new Vector2(xInput * currentSpeed, rb.linearVelocity.y);
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isCrouching", isCrouching);
     }
 
     private void Jump()
@@ -64,18 +97,13 @@ public class Player : MonoBehaviour
     private void HandleCollision()
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        headBlocked = Physics2D.Raycast(transform.position, Vector2.up, ceilingCheckDistance, whatIsGround);
     }
 
     private void HandleFlip()
     {
-        if (rb.linearVelocity.x > 0 && !facingRight)
-        {
-            Flip();
-        }
-        else if (rb.linearVelocity.x < 0 && facingRight)
-        {
-            Flip();
-        }
+        if (rb.linearVelocity.x > 0.1f && !facingRight) Flip();
+        else if (rb.linearVelocity.x < -0.1f && facingRight) Flip();
     }
 
     private void Flip()
@@ -86,6 +114,10 @@ public class Player : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance));
+        Gizmos.color = Color.red;
+        // Ground Check Ray
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundCheckDistance);
+        // Ceiling Check Ray
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up * ceilingCheckDistance);
     }
 }
